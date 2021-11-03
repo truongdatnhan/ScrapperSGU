c9a72f99faa05c2e7cbb56a42574fb9eaf19c2f5
Đạt Nhân Trương <truongdatnhan@gmail.com>
2021-11-03 18:05:38 +0700

    Add comment for ProgerssGraph

--- ScrapperSGU/src/main/java/gui/panel/ProgressGraphTab.java
+++ ScrapperSGU/src/main/java/gui/panel/ProgressGraphTab.java
@@ -1,7 +1,21 @@
 package gui.panel;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
 import javax.swing.JPanel;
 
+import net.miginfocom.swing.MigLayout;
+
 public class ProgressGraphTab extends JPanel {
+	
+	public ProgressGraphTab() {
+		setBackground(Color.WHITE);
+		setLayout(new BorderLayout());
+		
+		
+		//Tạo graph xong chỉ cần thay null add vào
+		add(null,BorderLayout.CENTER);
+	}
 	
 }
